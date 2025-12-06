9b2a96dba150aef066b3fef0b9b719f779573d74
Mateusz Pisanski <matpisak@gmail.com>
2025-12-06 19:17:32 +0100

    footer_update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -13,9 +13,11 @@ export function Footer() {
           {/* About */}
           <div>
             <div className="flex items-center gap-3 mb-4">
-              <div className="w-10 h-10 bg-accent rounded flex items-center justify-center">
-                <span className="text-accent-foreground font-bold">RL</span>
-              </div>
+          <img
+            src="/8.png"
+            alt="Radar Legislacyjny – logo"
+            className="w-10 h-10 lg:w-18 lg:h-18 rounded"
+          />
               <div>
                 <h3 className="font-bold">Radar Legislacyjny</h3>
               </div>
